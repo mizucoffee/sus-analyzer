53414fa06f761c05155e828d3653bcc260cfc8ec
Ritsuki Goto <develop@mizucoffee.net>
2018-12-31 01:48:30 +0900

    [refactor] 大幅な処理改善

--- score.js
+++ score.js
@@ -57,103 +57,19 @@ function getBEATs(validLines, measure) {
 }
 
 /**
- * susを解析する関数
- * @param {String} sus - sus
- * @return { measure: <小節数>, BPMs: [<BPMの配列>], BEATs: [拍数の配列], shortNotes: [{ measure: <小節番号>, lane_type: <レーン種別>, lane: <レーン番号>, note_type: <ノーツ種別>, position: <小節内のTick>, width: <ノーツ幅>}], longNotes: [{type: <レーン種別>, notes: [ measure: <小節番号>, lane_type: <レーン種別>, lane: <レーン番号>, defnum: <識別番号>, note_type: ノーツ種別, position: <小節内のTick>, width: <ノーツ幅> ]}]}
+ * ノーツからロングオブジェクトに変換する関数
+ * @param {Object} notes - ノーツ配列
+ * @param {number} type - ロング種別
+ * @return ロングオブジェクトの配列
  */
-function analyze(sus) {
-  const validLines = sus
-    .split('\n')
-    .filter(line => line.slice(0, 1) === '#')
-    .map(line => line.slice(1))
-  const data = {}
-
-  data.measure =
-    validLines
-      .filter(line => line.match(/^\d{3}[1-5][0-9a-fA-F][0-9a-zA-Z]?:/))
-      .map(line => Number(line.slice(0, 3)))
-      .reduce((a, b) => (a > b ? a : b), 0) + 1
-
-  data.BPMs = getBPMs(validLines, data.measure, meta.getMeta(sus).BASEBPM)
-  data.BEATs = getBEATs(validLines, data.measure)
-
-  data.shortNotes = validLines
-    .filter(line =>
-      line.match(/^\d{3}[1|5][0-9a-fA-F]:(\s*[0-9a-zA-Z][0-9a-gA-G])+\s*$/)
-    )
-    .map(line => ({
-      measure: Number(line.slice(0, 3)),
-      lane_type: Number(line.slice(3, 4)),
-      lane: parseInt(line.slice(4, 5), 16),
-      data: line
-        .split(':')[1]
-        .trim()
-        .replace(/ /g, ''),
-    }))
-    .reduce((list, line) => {
-      ;[...Array(line.data.length / 2)]
-        .map((note, i) => `${line.data[i * 2]}${line.data[i * 2 + 1]}`)
-        .map((note, i) => ({
-          pos: ((192 * data.BEATs[line.measure]) / (line.data.length / 2)) * i,
-          type: parseInt(note[0], 36),
-          width: parseInt(note[1], 17),
-        }))
-        .filter(note => note.width !== 0)
-        .map(note => ({
-          measure: line.measure,
-          lane_type: line.lane_type,
-          lane: line.lane,
-          note_type: note.type,
-          position: note.pos,
-          width: note.width,
-        }))
-        .forEach(e => list.push(e))
-      return list
-    }, [])
-
-  let longs = { 2: {}, 3: {}, 4: {} }
-  data.longNotes = validLines
-    .filter(line =>
-      line.match(
-        /^\d{3}[2-4][0-9a-fA-F][0-9a-zA-Z]:(\s*[0-9a-zA-Z][0-9a-gA-G])+\s*$/
-      )
-    )
-    .map(line => ({
-      measure: Number(line.slice(0, 3)),
-      lane_type: Number(line.slice(3, 4)),
-      lane: parseInt(line.slice(4, 5), 16),
-      defnum: parseInt(line.slice(5, 6), 36),
-      data: line
-        .split(':')[1]
-        .trim()
-        .replace(/ /g, ''),
-    }))
-    .reduce((list, line) => {
-      ;[...Array(line.data.length / 2)]
-        .map((note, i) => `${line.data[i * 2]}${line.data[i * 2 + 1]}`)
-        .map((note, i) => ({
-          pos: ((192 * data.BEATs[line.measure]) / (line.data.length / 2)) * i,
-          type: parseInt(note[0], 36),
-          width: parseInt(note[1], 17),
-        }))
-        .filter(note => note.width !== 0)
-        .map(note => ({
-          measure: line.measure,
-          lane_type: line.lane_type,
-          lane: line.lane,
-          defnum: line.defnum,
-          note_type: note.type,
-          position: Math.round(note.pos),
-          width: note.width,
-        }))
-        .forEach(e => list.push(e))
-      return list
-    }, [])
+function getLongLane(notes, type) {
+  let longs = {}
+  return notes
+    .filter(note => note.lane_type === type)
     .reduce((list, line) => {
       list[line.measure] = list[line.measure] || []
-      list[line.measure][line.position] =
-        list[line.measure][line.position] || []
-      list[line.measure][line.position].push(line)
+      list[line.measure][line.tick] = list[line.measure][line.tick] || []
+      list[line.measure][line.tick].push(line)
       return list
     }, [])
     .reduce((list, measure) => {
@@ -163,44 +79,106 @@ function analyze(sus) {
             .filter(note => type.includes(note.note_type))
             .forEach(note => {
               const lane = note.lane_type == 2 ? note.lane : 0
-              switch (note.note_type) {
-                case 1:
-                  if (!longs[note.lane_type].hasOwnProperty(lane))
-                    longs[note.lane_type][lane] = {}
-                  longs[note.lane_type][lane][note.defnum] = {
-                    type: note.lane_type,
-                    notes: [],
-                  }
-                  longs[note.lane_type][lane][note.defnum].notes.push(note)
-                  break
-                case 2:
-                  if (
-                    !longs[note.lane_type].hasOwnProperty(lane) ||
-                    !longs[note.lane_type][lane].hasOwnProperty(note.defnum)
-                  )
-                    break
-                  longs[note.lane_type][lane][note.defnum].notes.push(note)
-                  list.push(longs[note.lane_type][lane][note.defnum])
-                  delete longs[note.lane_type][lane][note.defnum]
-                  break
-                case 3:
-                case 4:
-                case 5:
-                  if (
-                    !longs[note.lane_type].hasOwnProperty(lane) ||
-                    !longs[note.lane_type][lane].hasOwnProperty(note.defnum)
-                  )
-                    break
-                  longs[note.lane_type][lane][note.defnum].notes.push(note)
-                  break
+              if ([2, 3, 4, 5].includes(note.note_type)) {
+                if (
+                  !longs.hasOwnProperty(lane) ||
+                  !longs[lane].hasOwnProperty(note.defnum)
+                ) {
+                  return
+                }
+                if ([3, 4, 5].includes(note.note_type)) {
+                  longs[lane][note.defnum].notes.push(note)
+                }
+                if (2 == note.note_type) {
+                  longs[lane][note.defnum].notes.push(note)
+                  list.push(longs[lane][note.defnum])
+                  delete longs[lane][note.defnum]
+                }
+              }
+              if (1 == note.note_type) {
+                longs[lane] = {}
+                longs[lane][note.defnum] = {
+                  type: note.lane_type,
+                  notes: [],
+                }
+                longs[lane][note.defnum].notes.push(note)
               }
             })
         })
       })
       return list
     }, [])
+}
 
-  return data
+/**
+ * susを解析する関数
+ * @param {String} sus - sus
+ * @param {Number} tickPerBeat - デフォルト値: 192
+ * @return { measure: <小節数>, BPMs: [<BPMの配列>], BEATs: [拍数の配列], shortNotes: [{ measure: <小節番号>, lane_type: <レーン種別>, lane: <レーン番号>, note_type: <ノーツ種別>, position: <小節内のTick>, width: <ノーツ幅>}], longNotes: [{type: <レーン種別>, notes: [ measure: <小節番号>, lane_type: <レーン種別>, lane: <レーン番号>, defnum: <識別番号>, note_type: ノーツ種別, position: <小節内のTick>, width: <ノーツ幅> ]}]}
+ */
+function analyze(sus, tickPerBeat = 192) {
+  const validLines = sus
+    .split('\n')
+    .filter(line => line.slice(0, 1) === '#')
+    .map(line => line.slice(1))
+  const score = {}
+
+  score.measure =
+    validLines
+      .filter(line => line.match(/^\d{3}[1-5][0-9a-fA-F][0-9a-zA-Z]?:/))
+      .map(line => Number(line.slice(0, 3)))
+      .reduce((a, b) => (a > b ? a : b), 0) + 1
+
+  score.BPMs = getBPMs(validLines, score.measure, meta.getMeta(sus).BASEBPM)
+  score.BEATs = getBEATs(validLines, score.measure)
+
+  const notes = validLines
+    .filter(line =>
+      line.match(
+        /^\d{3}[1-5][0-9a-fA-F][0-9a-zA-Z]?:(\s*[0-9a-zA-Z][0-9a-gA-G])+\s*$/
+      )
+    )
+    .map(line => line.split(':', 2))
+    .map(line => {
+      const data = {
+        measure: Number(line[0].slice(0, 3)),
+        lane_type: Number(line[0].slice(3, 4)),
+        lane: parseInt(line[0].slice(4, 5), 16),
+        data: line[1].trim().replace(/ /g, ''),
+      }
+      if (line[0].length === 6) data.channel = line[0].slice(5, 6)
+      return data
+    })
+    .reduce((list, line) => {
+      ;[...Array(line.data.length / 2)]
+        .map((note, i) => `${line.data[i * 2]}${line.data[i * 2 + 1]}`)
+        .map((note, i) => ({
+          ...line,
+          tick: Math.round(
+            ((tickPerBeat * score.BEATs[line.measure]) /
+              (line.data.length / 2)) *
+              i
+          ),
+          note_type: parseInt(note[0], 36),
+          width: parseInt(note[1], 17),
+        }))
+        .filter(note => note.width !== 0)
+        .map(note => {
+          delete note.data
+          return note
+        })
+        .forEach(e => list.push(e))
+      return list
+    }, [])
+
+  score.shortNotes = notes.filter(note => note.lane_type === 1)
+  score.airNotes = notes.filter(note => note.lane_type === 5)
+
+  score.holdNotes = getLongLane(notes, 2)
+  score.slideNotes = getLongLane(notes, 3)
+  score.airActionNotes = getLongLane(notes, 4)
+
+  return score
 }
 
 module.exports = {
--- test/test-score.js
+++ test/test-score.js
@@ -2,19 +2,23 @@ const fs = require('fs'),
   ava = require('ava'),
   SusAnalyzer = require('../index')
 
-const sus = fs.readFileSync('./test/template.sus', 'utf8');
+const sus = fs.readFileSync('./test/template.sus', 'utf8')
 const score = SusAnalyzer.getData(sus)
 
-ava('Measure',         t => t.is(score.measure                    , 21))
-ava('BPMs',            t => t.is(score.BPMs[3]                    , 80))
-ava('BEATs',           t => t.is(score.BEATs[3]                   , 3))
-ava('Short Measure',   t => t.is(score.shortNotes[8].measure      , 1))
-ava('Short Lane Type', t => t.is(score.shortNotes[8].lane_type    , 1))
-ava('Short Lane',      t => t.is(score.shortNotes[8].lane         , 4))
-ava('Short Note Type', t => t.is(score.shortNotes[8].note_type    , 2))
-ava('Short Position',  t => t.is(score.shortNotes[8].position     , 0))
-ava('Short Width',     t => t.is(score.shortNotes[8].width        , 4))
+ava('Measure', t => t.is(score.measure, 21))
+ava('BPMs', t => t.is(score.BPMs[3], 80))
+ava('BEATs', t => t.is(score.BEATs[3], 3))
+ava('Short Measure', t => t.is(score.shortNotes[8].measure, 1))
+ava('Short Lane Type', t => t.is(score.shortNotes[8].lane_type, 1))
+ava('Short Lane', t => t.is(score.shortNotes[8].lane, 4))
+ava('Short Note Type', t => t.is(score.shortNotes[8].note_type, 2))
+ava('Short Tick', t => t.is(score.shortNotes[8].tick, 0))
+ava('Short Width', t => t.is(score.shortNotes[8].width, 4))
 
-ava('Default BPM1',    t => t.is(SusAnalyzer.getData("#BPM01: 100\n#00308: 01\n#00510:00").BPMs[0]  , 120))
-ava('Default BPM2',    t => t.is(SusAnalyzer.getData("#00308: 01\n#00510:00").BPMs[0]  , 120))
-ava('Default BEATs',   t => t.is(SusAnalyzer.getData("").BEATs[0] , 4))
+ava('Default BPM1', t =>
+  t.is(SusAnalyzer.getData('#BPM01: 100\n#00308: 01\n#00510:00').BPMs[0], 120)
+)
+ava('Default BPM2', t =>
+  t.is(SusAnalyzer.getData('#00308: 01\n#00510:00').BPMs[0], 120)
+)
+ava('Default BEATs', t => t.is(SusAnalyzer.getData('').BEATs[0], 4))
